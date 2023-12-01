a82da3ea1728f2beaa1d4d552d1a7819a8565798
Solomon Gachure <gachuresolomon64@gmail.com>
2023-12-02 02:06:14 +0300

    Customized the navbar to a position fixed and added some customs to the page as well

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import {CiLight} from 'react-icons/ci'
+
 const Navbar = () => {
   return (
-    <div>
-        <nav>
-        <ul>
+    <div className='max-w-[1400px]  bg-stone-900 h-screen'>
+        <div className='p-2 w-full bg-transparent fixed flex items-center justify-between z-50 bg-opacity-60 backdrop-blur-lg backdrop-filter:blur(8px) bg-clip-padding'>
+         <div>
+{/**place logo here  */}
+         </div>
+         <div className='p-2'>
+               <nav className=' text-black flex gap-2 text-2xl'>
+        <ul className='gap-3 flex text-white font-bold'>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/contact">Contact</NavLink>
 
         </ul>
+        <div>
+            <CiLight size={30} className='text-white font-bold'/>
+            
+        </div>
 
     </nav>
+         </div>
+        </div>
     <main>
         <Outlet/>
     </main>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+        <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illo. Eaque fugit dicta suscipit quasi consequatur ratione inventore nihil alias at, asperiores aut rem, praesentium dolor iste minima quae quis!</p>
+    </div>
   )
 }
 
