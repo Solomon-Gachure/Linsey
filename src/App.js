4a198a044beb81f01bf28a736146de84ca55ac31
Solomon Gachure <gachuresolomon64@gmail.com>
2023-12-02 08:40:09 +0300

    Created an error page and customized

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} fr
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
+import Error from "./pages/error";
 
 function App() {
   const router= createBrowserRouter(
@@ -9,6 +10,8 @@ function App() {
 <Route path="/" element={<Navbar/>}>
       <Route index element={<Home/>} />
       <Route path="/contact" element={<Contact/>} />
+      <Route path="*" element={<Error/>} />
+
       </Route>
   ))
   return (
