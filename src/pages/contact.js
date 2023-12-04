2beafd894e00479e3b82b5b8c29e200e9bfcd342
Solomon Gachure <gachuresolomon64@gmail.com>
2023-12-04 13:14:38 +0300

    Made changes on the contact page and made it responsive as well

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,59 @@
 import React from 'react'
-
+import {FaFacebook, FaInstagram, FaTiktok, FaWhatsapp} from 'react-icons/fa'
+ 
 const Contact = () => {
+  const WhatsAppLink = ({ phoneNumber, message }) => {
+    return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  };
+  const whatsappLink = WhatsAppLink({ phoneNumber: "0798605270", message: "Hello welcome to Dee makeup!" });
   return (
-    <div>
-        <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, illo. Eaque fugit dicta suscipit quasi consequatur ratione inventore nihil alias at, asperiores aut rem, praesentium dolor iste minima quae quis!</p>
+    <div className='max-w-[1400px] h-full bg-stone-900 text-white p-2  font-bold pt-16'>
+      {/**About section */}
+       
+         <div className='border max-w-[1200px] rounded-lg bg-stone-900 flex flex-col justify-center items-center md:grid grid-cols-2 p-4 m-2  '>
+          <div className='h-[200px] w-[200px] md:h-[300px] md:w-[300px] border rounded-full  '>
+            <img 
+            className='h-full w-full object-cover rounded-full' 
+          src="https://img.freepik.com/free-photo/portrait-person-daily-life-new-york-city_23-2150820014.jpg?uid=R114683630&ga=GA1.1.134876288.1693412887&semt=ais_ai_generated"
+           alt="profile" />
+          </div>
+           <div className='flex flex-col border  items-center'>
+            <h1 className='font-bold text-teal-300 text-lg border-b'>Who I'm I?</h1>
+           <p className=''>
+            With unparalleled artistry and an exquisite eye for detail,
+             Dee stands as the epitome of an experienced makeup artist. Their adept hands effortlessly transform faces into canvases of beauty, showcasing a mastery of color, contour, and technique.
+              Dee's extensive knowledge of the latest trends merges seamlessly with a timeless elegance, ensuring clients radiate confidence. Beyond technical prowess, their exceptional interpersonal skills create an atmosphere of trust and collaboration, making each client feel truly seen and understood.
+             Dee is not just a makeup artist; they are a creator of confidence, a painter of self-expression, and an unparalleled professional in the world of beauty.
+           </p>
+           </div>
+        </div>
+       
+        {/**input section */}
+        <div className='bg-stone-900 flex flex-col justify-center items-center'>
+          <h1 className='text-xl text-teal-300'>Contact Me:</h1>
+          <div className='h-[300px] w-[300px]'>
+            <form action="https://getform.io/f/db96c5fb-9063-4883-a850-e833759e93be" method='POST' className='flex flex-col items-center h-full w-full  p-2 gap-2 text-black'>
+              <input className='w-[300px] p-2 outline-none bg-transparent text-white border' required type="text" name='name' placeholder='Enter Name' />
+              <input className='w-[300px] p-2 outline-none bg-transparent text-white border' required type="text" name='email' placeholder='Enter Email' />
+<textarea className='w-[300px] p-2 outline-none bg-transparent text-white border' required name="message" id="" cols="25" rows="5" placeholder='Enter Message'></textarea>
+<button className='text-white bg-green-500 p-2 w-[200px] font-bold rounded-lg text-xl'>Submit</button>
+            </form>
+          </div>
+        </div>
+        {/**Socials section */}
+        <div className='bg-stone-900 flex flex-col justify-center items-center'>
+          <h1>Find us through our socials:</h1>
+          <div className='flex justify-center items-center gap-14 p-2'>
+            {/**icons for the socials */}
+            <FaFacebook size={30} className='hover:scale-105 duration-300 cursor-pointer' />
+            <FaInstagram size={30} className='hover:scale-105 duration-300 cursor-pointer' />
+            <FaTiktok size={30} className='hover:scale-105 duration-300 cursor-pointer' />
+            <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+              <FaWhatsapp size={30} className='hover:scale-105 duration-300 cursor-pointer' a href='https://web.whatsapp.com/' />
+            </a>
+            
+          </div>
+        </div>
     </div>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,21 +2,26 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-white h-full top-2 max-w-[1400px] pt-16'>
-        <div>
+    <div className='text-white h-screen top-2 max-w-[1400px] pt-16'>
+        <div className='h-screen flex flex-col'>
           {/**home section */}
-          <div>
+          <div className=' bg-stone-900 w-full h-screen '>
             {/**hero section */}
-            <img 
+            
+              <img 
+            className='  w-full h-full object-contain top-0 left-0 '
             src="https://img.freepik.com/premium-photo/african-woman-afro-woman-beauty-concept-created-with-generative-ai-technology_185193-108150.jpg?size=626&ext=jpg&uid=R114683630&ga=GA1.1.134876288.1693412887&semt=ais"
              alt="hero" />
-             <div>
+            
+             <div className='  flex flex-col h-screen  items-center justify-center'>
 <h1>Welcome to Dee Makeup</h1>
 <p>We  welcome you to the best makeup artists in town</p>
 <p>We offer a variety of makeup services from our experienced staff.</p>
              </div>
           </div>
-<div>
+
+        </div>
+        <div className='h-screen  bg-stone-900'>
   <h1>Services we offer:</h1>
   <div>
     <h1>Eye Services</h1>
@@ -25,7 +30,6 @@ const Home = () => {
     </p>
   </div>
 </div>
-        </div>
     </div>
   )
 }
