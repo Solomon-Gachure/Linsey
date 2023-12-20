1e702f8d2cb920a51520705ec209cc638dc9bdc6
Solomon Gachure <gachuresolomon64@gmail.com>
2023-12-20 14:38:45 +0300

    Some slight changes on the nails page

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -56,9 +56,9 @@ const handleAmount=()=>{
 {/**pop up message to confirm payment */}
 {
     popup&&(
-        <div className='fixed w-[500px] h-[300px]   bg-transparent z-50 bg-opacity-60 backdrop-blur-lg backdrop-filter:blur(8px) bg-clip-padding pt-16 flex items-center justify-center text-stone-900 font-bold border'>
-            <div className='w-full h-full flex flex-col justify-center items-center  '>
-                <h1 className='text-2xl'>You are about to pay</h1>
+        <div className='fixed w-[350px] h-[400px] md:w-[500px] md:h-[450px]  border-black   bg-transparent z-50 bg-opacity-60 backdrop-blur-lg backdrop-filter:blur(8px) bg-clip-padding pt-16 flex items-center justify-center text-stone-900 font-bold border'>
+            <div className='w-full h-full flex flex-col justify-center items-center gap-2  '>
+                <h1 className='text-2xl font-bold'>You are about to pay</h1>
                 <div className='flex items-center gap-4'>
                     <button onClick={handleAmount} className='bg-green-500 p-2 text-white font-bold w-[50px] rounded-lg hover:scale-105 duration-300'>YES</button>
                 <button onClick={closePop} className='bg-red-500 p-2 text-white font-bold w-[50px] rounded-lg hover:scale-105 duration-300'>NO</button>
@@ -71,11 +71,11 @@ const handleAmount=()=>{
 {/**amount to be placed pop up */}
 {
     amount&&(
-        <div className='fixed w-[500px] h-[500px]  bg-transparent z-50 bg-opacity-60 backdrop-blur-lg backdrop-filter:blur(8px) bg-clip-padding pt-16 flex items-center justify-center text-stone-900 font-bold border'>
+        <div className='fixed w-[350px] h-[400px] md:w-[500px] md:h-[450px]  border-black  bg-transparent z-50 bg-opacity-60 backdrop-blur-lg backdrop-filter:blur(8px) bg-clip-padding pt-16 flex items-center justify-center text-stone-900 font-bold border'>
             <div className='w-full h-full flex flex-col justify-center items-center  '>
                 <form className='flex flex-col gap-2' action="https://getform.io/f/db96c5fb-9063-4883-a850-e833759e93be" method="post">
 
-<input className='p-2 bg-transparent text-white font-bold outline-none  border-2 border-stone-900/70 rounded-lg' type="text" placeholder='Enter Amount' />
+<input name='amount' className='p-2 bg-transparent text-white font-bold outline-none  border-2 border-stone-900/70 rounded-lg' type="text" placeholder='Enter Amount' required />
 
 <button className='bg-green-500 p-2 text-white font-bold w-[250px] rounded-lg hover:scale-105 duration-300'>Pay</button>
                 </form>
